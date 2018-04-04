253e76d9422e61ecb1a0e12fad66b0bf2eff2dfd
Sg-23 <sonalvarshney9@gmail.com>
2018-04-04 14:59:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sonal/mint/Userabout.java
@@ -0,0 +1,18 @@
+package com.example.sonal.mint;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sonal on 10-03-2018.
+ */
+
+public class Userabout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userabout);
+    }
+}
